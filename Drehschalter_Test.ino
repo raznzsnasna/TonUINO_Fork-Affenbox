fa8cb945b49295d17a33d69b7af55cba543a1769
marco-117 <56135788+marco-117@users.noreply.github.com>
2020-03-10 11:14:05 +0100

    Update Drehschalter_Test.ino

--- Drehschalter_Test.ino
+++ Drehschalter_Test.ino
@@ -1,1 +1,18 @@
+#define ROT_SWITCH_POS_NUM 12
+#define ROT_SWITCH_PIN  A0
+
+uint8_t currentPos = 0;
+
+uint8_t getSwitchPosition ()
+{
+  float analogValue = analogRead(ROT_SWITCH_PIN);;
+  float stepMin = (5/ROT_SWITCH_POS_NUM) - ((5/ROT_SWITCH_POS_NUM)*0.15);
+  float stepMax = (5/ROT_SWITCH_POS_NUM) - ((5/ROT_SWITCH_POS_NUM)*0.15);
+  
+   for (uint8_t x = 0; x <= ROT_SWITCH_POS_NUM; x++)
+  {
+    if (analogValue > stepMin*x || analogValue < stepMax*x)
+      return x;
+  }
+}
 
